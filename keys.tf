35e9567da7d589990b09f8b7184902f3ab37f560
anjali3soni <anjalisoniu12@gmail.com>
2024-12-16 19:15:14 +0530

    Updated keys.tf

--- keys.tf
+++ keys.tf
@@ -1,4 +1,3 @@
-## SSh Key Pair for Bastion Host
 resource "aws_key_pair" "bastion" {
   key_name   = "${local.env.environment}-bastion-key"
   public_key = file("${terraform.workspace}/bastion.pub")
@@ -6,7 +5,6 @@ resource "aws_key_pair" "bastion" {
   depends_on = [aws_security_group.bastion]
 }
 
-### SSH Key Pair for Instances
 resource "aws_key_pair" "instance" {
   key_name   = "${local.env.environment}-instance-key"
   public_key = file("${terraform.workspace}/instance.pub")
