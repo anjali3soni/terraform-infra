f36ca6077c4636d6e5f1f6722c0609537f19bc25
anjali3soni <anjalisoniu12@gmail.com>
2024-12-16 17:48:19 +0530

    Updated files

--- acm-listner.tf
+++
@@ -1,5 +0,0 @@
-# ### Attaching acm certificate to 443 listener
-# resource "aws_lb_listener_certificate" "alb_listener_certificate" {
-#   listener_arn    = aws_lb_listener.alb_default_listener.arn
-#   certificate_arn = aws_acm_certificate.acm_mena.arn
-# }
