8b5b4cdc8e3b41006527f48013596395f769ebe0
anjali3soni <anjalisoniu12@gmail.com>
2024-12-16 18:00:35 +0530

    Updated alb-listner.tf

--- alb-listner.tf
+++ alb-listner.tf
@@ -1,6 +1,3 @@
-## ## --------- Default Listner ----------
-## Creating default alb listener 
-### Default_lister -> 443
 resource "aws_lb_listener" "alb_default_listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "443"
@@ -27,7 +24,6 @@ resource "aws_lb_listener" "alb_default_listener" {
 }
 
 
-### --------- ALB listner port 80 to 443 ----------
 resource "aws_lb_listener" "alb_http_to_https" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "80"
@@ -40,44 +36,6 @@ resource "aws_lb_listener" "alb_http_to_https" {
       port        = "443"
       protocol    = "HTTPS"
       status_code = "HTTP_301"
-    }
-  }
-}
-
-
-### --------- ALB listner ocpp wss port 8080 ----------
-resource "aws_lb_listener" "alb_ocpp_ws" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = "8080"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "application/json"
-      message_body = "Please check context path"
-      status_code  = "503"
-    }
-  }
-}
-
-
-
-### --------- ALB listner ocpp wss port 8001 ----------
-
-resource "aws_lb_listener" "alb_ocpp_ws_2" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = "8001"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "application/json"
-      message_body = "Please check context path"
-      status_code  = "503"
     }
   }
 }
